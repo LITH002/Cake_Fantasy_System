b6340cc6ba70f970ed8b65ee7f80fff91ff12776
LITH002 <liteenilanjith@gmail.com>
2025-05-08 14:07:52 +0530

    FE-BE Cart Functionality Integration

--- Frontend/src/components/ItemDisplay/ItemDisplay.jsx
+++ Frontend/src/components/ItemDisplay/ItemDisplay.jsx
@@ -11,7 +11,7 @@ const ItemDisplay = ({category}) => {
         <div className="item-display-list">
           {item_list.map((item,index)=>{
             if (category==="All" || category===item.category) {
-              return <Items key={index} id={item._id} name={item.name} price={item.price} description={item.description} image={item.image}/>
+              return <Items key={index} id={item.id} name={item.name} price={item.price} description={item.description} image={item.image}/>
             }
           })}
         </div>
